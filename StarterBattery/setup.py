28ae3234d8cddc682e5d6094a5150036d4c58abc
roadbuck <164482499+roadbuck@users.noreply.github.com>
2024-03-22 16:08:50 +0100

    Update setup.py

--- StarterBattery/setup.py
+++ StarterBattery/setup.py
@@ -1,1 +1,8 @@
+import subprocess
+import os
+
+with open('/data/RoadbuckMods/StarterBattery/files/dbus.ini', 'r') as file:
+    for line in file:
+        command = line.strip()
+        subprocess.run(command, shell=True)
 
