8b8350bc403c9f6bdabf6a8dd4edb3d6fa0f9bbd
roadbuck <164482499+roadbuck@users.noreply.github.com>
2024-04-05 00:28:43 +0200

    Add files via upload

---
+++ setup-de.py
@@ -0,0 +1,117 @@
+import subprocess
+import os
+import shutil
+from datetime import datetime
+
+directories = [
+    '/data/RoadbuckMods',
+    '/data/RoadbuckMods/StarterBattery',
+    '/data/RoadbuckMods/StarterBattery/backup_files',
+]
+
+for directory in directories:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print(f"Verzeichnis {directory} erstellt.")
+    else:
+        print(f"Verzeichnis {directory} existiert bereits.")
+
+source_directory = '/data/StarterBattery/files'
+destination_directory = '/data/RoadbuckMods/StarterBattery'
+
+try:
+    shutil.move(source_directory, destination_directory)
+    print("Verzeichnis erfolgreich verschoben.")
+except Exception as e:
+    print(f"Fehler beim Verschieben des Verzeichnisses: {e}")
+
+def set_file_permissions(file_path, permissions):
+    try:
+        os.chmod(file_path, permissions)
+        print(f"Berechtigungen für {file_path} auf {permissions} gesetzt.")
+    except Exception as e:
+        print(f"Error setting permissions for {file_path}: {e}")
+
+def run_bash_script(script_path):
+    try:
+        subprocess.run([script_path], check=True)
+        print(f"Script {script_path} executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing script {script_path}: {e}")
+
+if __name__ == "__main__":
+    file_path = "/data/RoadbuckMods/StarterBattery/files/update_dbus_settings.sh"
+    permissions = 0o755
+
+    set_file_permissions(file_path, permissions)
+    run_bash_script(file_path)
+
+files_to_copy = [
+    {
+        'source': '/data/RoadbuckMods/StarterBattery/files/qml/RB_StarterBattery.qml',
+        'destination': '/opt/victronenergy/gui/qml/RB_StarterBattery.qml'
+    },
+    {
+        'source': '/data/RoadbuckMods/StarterBattery/files/qml/DE_PageSettingsRoadbuckMods.qml',
+        'destination': '/opt/victronenergy/gui/qml/PageSettingsRoadbuckMods.qml'
+    },
+    {
+        'source': '/data/RoadbuckMods/StarterBattery/files/qml/DE_PageSettingsRoadbuckStarterBattery.qml',
+        'destination': '/opt/victronenergy/gui/qml/PageSettingsRoadbuckStarterBattery.qml'
+    },
+    {
+        'source': '/data/RoadbuckMods/StarterBattery/files/qml/DE_PageSettingsRoadbuckTruma.qml',
+        'destination': '/opt/victronenergy/gui/qml/PageSettingsRoadbuckTruma.qml'
+    },
+    {
+        'source': '/data/RoadbuckMods/StarterBattery/files/qml/DE_PageSettingsRoadbuckMaxxFan.qml',
+        'destination': '/opt/victronenergy/gui/qml/PageSettingsRoadbuckMaxxFan.qml'
+    },
+    {
+        'source': '/data/RoadbuckMods/StarterBattery/files/qml/DE_PageSettingsRoadbuckShelly.qml',
+        'destination': '/opt/victronenergy/gui/qml/PageSettingsRoadbuckShelly.qml'
+    },
+    {
+        'source': '/data/RoadbuckMods/StarterBattery/files/qml/DE_PageSettingsRoadbuckTeltonika.qml',
+        'destination': '/opt/victronenergy/gui/qml/PageSettingsRoadbuckTeltonika.qml'
+    },
+    {
+        'source': '/data/RoadbuckMods/StarterBattery/files/qml/DE_PageSettingsRoadbuckWeather.qml',
+        'destination': '/opt/victronenergy/gui/qml/PageSettingsRoadbuckWeather.qml'
+    },
+]
+
+for file_info in files_to_copy:
+    source_path = file_info['source']
+    destination_path = file_info['destination']
+
+    if os.path.exists(destination_path):
+        os.rename(destination_path, destination_path + '.orig')
+        print(f"Die existierende Datei wurde umbenannt zu {destination_path}.orig")
+
+    shutil.copy(source_path, destination_path)
+    print(f"Die Datei wurde von {source_path} nach {destination_path} kopiert")
+
+source_files = [
+    '/opt/victronenergy/gui/qml/main.qml',
+    '/opt/victronenergy/gui/qml/OverviewHub.qml',
+    '/opt/victronenergy/gui/qml/OverviewHubEnhanced.qml',
+    '/opt/victronenergy/gui/qml/PageSettingsDisplay.qml',
+    '/opt/victronenergy/gui/qml/PageSettingsRoadbuckStarterBattery.qml',
+    '/opt/victronenergy/gui/qml/RB_StarterBattery.qml',
+]
+
+destination_directory = '/data/RoadbuckMods/StarterBattery/backup_files'
+
+current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M')
+destination_directory_with_timestamp = os.path.join(destination_directory, current_datetime)
+
+if not os.path.exists(destination_directory_with_timestamp):
+    os.makedirs(destination_directory_with_timestamp)
+    print(f"Verzeichnis {destination_directory_with_timestamp} angelegt")
+
+for file_path in source_files:
+    file_name = os.path.basename(file_path)
+    destination_path = os.path.join(destination_directory_with_timestamp, file_name)
+    shutil.copy(file_path, destination_path)
+    print(f"Dateo {file_path} kopiert nach {destination_path}")
